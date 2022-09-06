20810753a614cb90417963ff82b08011986bc29f
trautmann.marton <trautmann.marton@outlook.com>
2022-09-06 14:43:43 +0200

    fix register.html

--- data_manager.py
+++ data_manager.py
@@ -43,18 +43,9 @@ def get_connection_data(db_name=None):
 
 
 def execute_select(statement, variables=None, fetchall=True):
-    """
-    Execute SELECT statement optionally parameterized.
-    Use fetchall=False to get back one value (fetchone)
-
-    Example:
-    > execute_select('SELECT %(title)s; FROM shows', variables={'title': 'Codecool'})
-    statement: SELECT statement
-    variables:  optional parameter dict, optional parameter fetchall"""
     result_set = []
     with establish_connection() as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
             cursor.execute(statement, variables)
             result_set = cursor.fetchall() if fetchall else cursor.fetchone()
     return result_set
-
